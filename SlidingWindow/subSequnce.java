e338c4147c046a7ba6f4494af8adc19a53a85ca1
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-07-13 23:55:54 +0530

    find the highest subsequnce

---
+++ SlidingWindow/subSequnce.java
@@ -0,0 +1,11 @@
+package SlidingWindow;
+
+public class subSequnce {
+    public static void main(String[] args) {
+    int[] arr = {1,2,3,4};
+    int n= arr.length;
+    for(int i=0 ; i< n; i++){
+        
+    }
+    }
+}
